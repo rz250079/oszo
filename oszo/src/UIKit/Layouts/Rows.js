a8c1ba9a338fba312588c612127f59b14e1a6d0d
ob250080 <ob250080@ncr.com>
2021-07-27 18:51:36 +0300

    add receipt design

--- oszo/src/UIKit/Layouts/Rows.js
+++ oszo/src/UIKit/Layouts/Rows.js
@@ -2,7 +2,7 @@ import './Rows.css';
 
 const Rows = (props) => {
     return (
-        <div className="Rows" justify={props.justify}>
+        <div className="Rows" justify={props.justify} extra={props.extra ? 'true' : ''}>
             {props.children}
         </div>
     )
--- oszo/src/View/Receipt.js
+++ oszo/src/View/Receipt.js
@@ -1,4 +1,5 @@
-import { Rows, Line, Logo } from 'UIKit';
+import { Icon,Rows, Line, Logo } from 'UIKit';
+import "./Receipt.css";
 
 
 const Reciept = () => {
@@ -6,7 +7,7 @@ const Reciept = () => {
     
     return (
         <div className="View Receipt">
-            <Rows>
+            <Rows extra>
                 <Line>
                     <Logo />
                 </Line>
@@ -21,7 +22,71 @@ const Reciept = () => {
                         <h3>123 Main Street</h3>
                     </Rows>
                 </Line>
-            </Rows>
+                <Line>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>QTY</th>
+                                <th>DESCRIPTION</th>
+                                <th>PRICE</th>
+                                <th>SUBTOTAL</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>2</td>
+                                <td>Blue large Widgets</td>
+                                <td>15$</td>
+                                <td><strong>15$</strong></td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>Black large Widgets</td>
+                                <td>5$</td>
+                                <td><strong>1500$</strong></td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>White large Widgets</td>
+                                <td>555$</td>
+                                <td><strong>1565$</strong></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Line>
+                <Line>
+                    <table className="secondTable">
+                        <thead>
+                            <tr>
+                                <th>PAYMENT INFO</th>
+                                <th>DUE BY</th>
+                                <th>TOTAL DUE</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <Rows>
+                                        <h4>Account No:<b>10000987</b></h4>
+                                        <h4>Routing No: <b>10000987</b></h4>
+                                    </Rows>
+                                </td>
+                                <td>
+                                    <h1><b>May 30,2024</b></h1>
+                                </td>
+                                <td>
+                                    <h1 className="red"><strong>105.00$</strong></h1>
+                                </td>
+                            </tr>
+                            
+                       </tbody>
+                    </table>
+                </Line>
+                <hr></hr>
+                <Line>
+                        <Icon i="heart" />  <h3><strong>Thank you!</strong></h3>
+                    </Line>
+           </Rows>
         </div>
     )
 }
